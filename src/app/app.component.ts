0c1b32ea75b023a5859732ea37167bfeeeec1533
Andriy Orehov <andriyorehov@gmail.com>
2020-01-31 11:58:54 +0200

    add ability to add field fix order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import { Validators } from '@angular/forms';
 
 
@@ -11,37 +11,39 @@ import { Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   formFields = [1];
   bioSection: FormGroup;
-
-  formConfig = {
-    firstName: {title: 'First Name'},
-    lastName: {title: 'Last Name'},
-    age: {title: 'Age'}
-  };
-
-
-  getControl(config) {
-    const form = {};
-    for (const i in config) {
-      if (config.hasOwnProperty(i)) {
-        form[i] = [''];
-      }
-    }
-    return form;
-  }
+  formConfig = [
+    {control: 'firstName', title: 'First Name'},
+    {control: 'lastName', title: 'Last Name'},
+    {control: 'age', title: 'Age'}
+  ];
 
   constructor(private fb: FormBuilder) {
     this.bioSection = this.fb.group(this.getControl(this.formConfig));
+    console.log(this.bioSection);
+  }
+
+  ngOnInit() {
+  }
+
+  getControl(config) {
+    const form = {};
+    for (const i of config) {
+      form[i.control] = [''];
+    }
+    return form;
   }
 
   callingFunction() {
     console.log(this.bioSection.value);
   }
 
-  ngOnInit() {
-  }
-
   addFormField() {
     this.formFields.push(1);
+  }
+
+  addFormFieldBuilder() {
+    this.formConfig = [...this.formConfig,  {control: 'email', title: 'EMail'}];
+    this.bioSection = this.fb.group(this.getControl(this.formConfig));
   }
 
 }
